5a8a5dcb4b610445c1cbe837204ec32735b25a8b
Jeff-Maina <devyoungblud@gmail.com>
2024-04-14 21:35:32 +0300

    New item button

--- components/dashboard/CreateButton.vue
+++ components/dashboard/CreateButton.vue
@@ -1,17 +1,30 @@
 <script setup lang="ts">
-import { X } from "lucide-vue-next";
+import { Plus } from "lucide-vue-next";
 
 const isCreatingNewItem = ref(false);
 const toggleCreatingNewItem: () => void = () =>
   (isCreatingNewItem.value = !isCreatingNewItem.value);
-  
+const closeMenu: () => void = () => (isCreatingNewItem.value = false);
 </script>
 
 <template>
   <div class="relative">
-    <button class="size-10 rounded-full bg-black grid place-items-center">
-      <Plus class="stroke-white md:size-5" stroke-width="3" />
+    <button
+      @click="toggleCreatingNewItem"
+      class="size-10 rounded-full transition-all duration-500 grid place-items-center relative bg-black"
+      :class="isCreatingNewItem ? 'z-[999]':''"
+    >
+      <div
+        :class="isCreatingNewItem ? 'rotate-45' : 'rotate-0'"
+        class="transition-all duration-100"
+      >
+        <Plus class="stroke-white md:size-5" stroke-width="3" />
+      </div>
     </button>
+    <DashboardMenuNewItemMenu
+      :isCreatingNewItem="isCreatingNewItem"
+      @closeMenu="closeMenu"
+    />
   </div>
 </template>
 
---
+++ components/dashboard/menu/NewItemMenu.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { FilePlus, FolderPlus } from "lucide-vue-next";
+const props = defineProps({
+  isCreatingNewItem: {
+    type: Boolean,
+  },
+});
+
+const emits = defineEmits(["closeMenu"]);
+</script>
+
+<template>
+  <UiMask :isMaskActive="isCreatingNewItem" @toggleMask="$emit('closeMenu')" />
+  <Transition name="menu_slide">
+    <div
+      v-if="isCreatingNewItem"
+      class="absolute top-[150%] right-0 w-52 bg-white shadow rounded-3xl z-[999] flex flex-col divide divide-y divide-neutral-100 md:divide-neutral-200 overflow-hidden font-normal"
+    >
+      <button
+        class="h-14 w-full px-5 flex items-center gap-3 hover:bg-neutral-100 transition-all duration-100"
+      >
+        <div><FilePlus :size="18" /></div>
+        <span>New file</span>
+      </button>
+      <button
+        class="h-14 w-full px-5 flex items-center gap-3 hover:bg-neutral-100 transition-all duration-100"
+      >
+        <div><FolderPlus :size="18" /></div>
+        <span>New Folder</span>
+      </button>
+    </div>
+  </Transition>
+</template>
+
+<style></style>
