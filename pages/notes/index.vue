c8a59d94f3121718ced140f7e80aace8ed198cf2
Jeff-Maina <devyoungblud@gmail.com>
2024-04-17 01:55:45 +0300

    codeblock element

--- pages/notes/index.vue
+++ pages/notes/index.vue
@@ -167,6 +167,11 @@ const closeCategoryMenu = () => (isCategoryMenuShowing.value = false);
                 </div>
               </div>
             </div>
+            <!-- image -->
+            <div class="w-full rounded-3xl overflow-hidden">
+              <img src="https://i.pinimg.com/564x/20/e5/f6/20e5f69171e58776fbd0d99916459766.jpg" class="w-full object-cover" alt="" />
+            </div>
+            <!-- codeblock -->
           </div>
           <div class="w-full col-span-1"></div>
           <div class="w-full col-span-1"></div>
