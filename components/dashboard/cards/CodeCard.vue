1163379a41b0fa2700d756315e9ff44d599c8f6b
Jeff-Maina <devyoungblud@gmail.com>
2024-04-18 20:06:22 +0300

    adjust scroll bar styling

--- components/dashboard/cards/CodeCard.vue
+++ components/dashboard/cards/CodeCard.vue
@@ -33,18 +33,36 @@ print(data)
 
 <template>
   <div
-    class="w-full rounded-xl bg-[#fff] border border-neutral-300/60 relative group/codecard overflow-hidden pr-3"
+    class="w-full rounded-xl codecard bg-[#fff] border border-neutral-300/60 relative group/codecard overflow-hidden pr-3 p-3"
   >
     <button
       class="size-8 absolute top-3 right-3 bg-neutral-100 border border-neutral-200 z-30 rounded-lg opacity-0 group-hover/codecard:opacity-[1] transition-all duration-200 grid place-items-center"
     >
       <Clipboard :size="16" class="stroke-neutral-600" />
     </button>
-    <Shiki  lang="python" :code="code" class="px-6 py-4 !text-sm w-full  overflow-scroll" hight />
+    <Shiki
+      lang="python"
+      :code="code"
+      class="px-3 py-1 !text-sm w-full overflow-scroll shiki_block"
+      hight
+    />
   </div>
 </template>
 
-<style></style>
-
+<style>
+.shiki_block::-webkit-scrollbar {
+  background: transparent !important;
+  width: 10px;
+  height: 5px;
+}
+.shiki_block::-webkit-scrollbar-thumb {
+  /* background: #acacac !important; */
+  background: #fff !important;
+  border-radius: 10px;
+}
+.shiki_block:hover::-webkit-scrollbar-thumb {
+  background: #bdbdbd !important;
+}
+</style>
 
 <!-- 'catppuccin-latte' - [#EFF1F5]-->
